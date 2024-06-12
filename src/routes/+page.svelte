08f4195c62443edb50390e0c102e07c620010e08
Felix Sargent <felix.sargent@gmail.com>
2024-06-12 11:38:23 -0700

    Slim packages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,8 +12,7 @@
   <div class="row">
     <div class="leftCol">
       <div class="description">
-        <h1>approval.vote</h1>
-        : detailed reports on approval voting elections.
+        <h1>approval.vote</h1>: detailed reports on approval voting elections.
       </div>
       <p>
         In an <a href="https://en.wikipedia.org/wiki/Approval_Voting">
