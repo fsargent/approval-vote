cc4700ebeca7ba67c85fe2a3141163bfb229d336
fsargent <felix.sargent@gmail.com>
2024-06-12 19:09:47 +0000

    Deploying to gh-pages from @ electionscience/approval-vote@af2a4fff15362cbfa7cac82fb528bb1d12cac865 🚀

---
+++ _app/immutable/nodes/3.DT1EhKn9.js
@@ -0,0 +1,15 @@
+import{s as v,n as o}from"../chunks/scheduler._RQNZmqd.js";import{S as f,i as b,e as h,s as m,c as d,g as c,a as g,f as y,h as n,d as r}from"../chunks/index.BquU_noq.js";import{b as w}from"../chunks/paths.BJtIuP58.js";function _(u){let e,l="About approval.vote",s,a,p=`<div class="description"><h1>About <a href="${w}/">approval.vote</a></h1></div> <p><a href="https://en.wikipedia.org/wiki/Approval_Voting">Approval Voting elections</a>
+  produce a lot of data about voter preferences. Typically, this information is
+  made available by election boards in specialized file formats and requires
+  processing to interpret. The purpose of approval.vote is to increase transparency into these
+  elections by creating detailed and intuitive reports about them.</p> <p>Approval voting is a voting system that allows voters to vote for as many
+  candidates as they like. It is a simple and intuitive system that
+  produces results that are easy to understand and verify.</p> <p>All reports are generated from the raw ballot data, rather than using the
+  official tabulation. As a result, the reports also serve as an independent
+  tabulation of the results that can be reproduced using published source code.</p> <p>approval.vote is maintained by
+   <a href="https://felixsargent.com">Felix Sargent</a>. It is a fork from the original work of <a href="https://paulbutler.org">Paul Butler&#39;s</a> <a href="https://ranked.vote">ranked.vote</a>.
+  It is non-partisan and has received no outside funding.</p> <h2>License</h2> <p>This website and the data pipeline behind it (including the tabulator) are
+  <a href="https://github.com/electionscience/approval-vote">open source</a> and liberally licensed. Website content and generated reports may
+  be freely distributed with attribution under the <a href="https://creativecommons.org/licenses/by/2.0/">CC-BY</a> license.</p> <h2>Colophon</h2> <p>This website, including
+  visualizations, was created with <a href="https://svelte.dev/">Svelte</a>, and is hosted on <a href="https://github.com/electionscience/approval-vote">Github Pages</a>.
+  The font is <a href="https://github.com/adobe-fonts/source-sans-pro">Source Sans Pro</a>.</p>`;return{c(){e=h("title"),e.textContent=l,s=m(),a=h("div"),a.innerHTML=p,this.h()},l(t){e=d(t,"TITLE",{"data-svelte-h":!0}),c(e)!=="svelte-3g790q"&&(e.textContent=l),s=g(t),a=d(t,"DIV",{class:!0,"data-svelte-h":!0}),c(a)!=="svelte-19jydcv"&&(a.innerHTML=p),this.h()},h(){y(a,"class","container")},m(t,i){n(t,e,i),n(t,s,i),n(t,a,i)},p:o,i:o,o,d(t){t&&(r(e),r(s),r(a))}}}class k extends f{constructor(e){super(),b(this,e,null,_,v,{})}}export{k as component};
