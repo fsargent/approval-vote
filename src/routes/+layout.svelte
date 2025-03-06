fd7fd552ab60dc9e7462c680135d1c38e8b59434
Felix Sargent <felix.sargent@gmail.com>
2025-03-06 12:13:09 +0000

    Improve page loads by deferring fonts.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,10 +8,11 @@
 	let { children }: Props = $props();
 </script>
 <svelte:head>
-	<link rel="stylesheet" href="{base}/style.css" />
-	<link rel="icon" type="image/png" href="{base}/icons/icon.png" />
+	<!-- Preload font CSS first -->
+	<link rel="preload" href="{base}/fonts.css" as="style" />
+	<link rel="stylesheet" href="{base}/fonts.css" />
 
-	<!-- Preload critical font files -->
+	<!-- Then preload the most critical font file -->
 	<link
 		rel="preload"
 		href="https://fonts.gstatic.com/s/sourcesanspro/v22/6xK3dSBYKcSV-LCoeQqfX1RYOo3qOK7l.woff2"
@@ -20,7 +21,11 @@
 		crossorigin
 	/>
 
-	<!-- Your existing meta tags -->
+	<!-- Regular CSS after fonts are handled -->
+	<link rel="stylesheet" href="{base}/style.css" />
+	<link rel="icon" type="image/png" href="{base}/icons/icon.png" />
+
+	<!-- Rest of your head content -->
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<meta name="robots" content="index, follow" />
 	<meta name="author" content="Felix Sargent" />
