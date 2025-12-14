cca179671e67ff0a19ee12832106ae9c5bcfd695
fsargent <felix.sargent@snyk.io>
2025-12-14 23:14:37 +0000

    Deploying to gh-pages from @ fsargent/approval-vote@c2ee7e16707c666761ae7e6a2a186e809d849160 🚀

--- _app/immutable/entry/start.B-77sAwQ.js
+++
@@ -1,1 +0,0 @@
-import{bG as s,bF as a}from"../chunks/B6GVoWwO.js";export{s as load_css,a as start};
---
+++ _app/immutable/entry/start.CK3haVjT.js
@@ -0,0 +1,1 @@
+import{bG as s,bF as a}from"../chunks/B-bUwNvh.js";export{s as load_css,a as start};
